435d9cad32f69a50d82dfaf9adfeb27eaa7a2e21
Glauco Gonçalves <glaestgon@gmail.com>
2016-07-14 23:26:35 -0300

    ajustes de formato do codigo

--- robot-joystick.py
+++ robot-joystick.py
@@ -2,11 +2,6 @@ import pygame
 import serial
 import threading
 
-direction = 'n'
-
-ser = serial.Serial("/dev/tty.HC-05-DevB", writeTimeout = 0.1)
-ser.baudrate = 9600
-
 def do_every (interval):
 	global direction
 	global ser
@@ -14,30 +9,26 @@ def do_every (interval):
     		ser.write(direction)
 		threading.Timer (interval, do_every, [interval]).start()
 
-pygame.init()
+if __name__ == '__main__':
+	direction = 'n'
 
-# d = {0: "w", 1: "d", 2: "s", 3: "a"}
-directions = {(3,1):'a',(3,-1):'d',(3,0):'x',(4,1):'w',(4,-1):'s',(4,0):'y'}
+	ser = serial.Serial("/dev/tty.HC-05-DevB", writeTimeout = 0.1)
+	ser.baudrate = 9600
+	done = False
+	directions = {(3,1):'a',(3,-1):'d',(3,0):'x',(4,1):'w',(4,-1):'s',(4,0):'y'}
+	pygame.init()
 
-done = False
+	joystick = pygame.joystick.Joystick(0)
+	joystick.init()
 
-joystick = pygame.joystick.Joystick(0)
-joystick.init()
-print(joystick.get_numaxes())
-
-while done is False:
-  #for event in pygame.event.get(): # User did something
-	event = pygame.event.wait()
+	while done is False:
+		event = pygame.event.wait()
 	
-	if event.type in [pygame.JOYBUTTONDOWN]:
-		# print("Joystick button pressed.")
-		# print(event.dict)
-		btn = event.dict['button']
-		if (btn is 8):
-			done = True
-		#elif btn in d.keys():
-		#	ser.write(d[btn])
-	if event.type in [pygame.JOYAXISMOTION]:
-		direction = directions.get((event.axis,int(event.value)),'n')
-		do_every(0.1)
-ser.close()
+		if event.type in [pygame.JOYBUTTONDOWN]:
+			btn = event.dict['button']
+			if (btn is 8):
+				done = True
+		if event.type in [pygame.JOYAXISMOTION]:
+			direction = directions.get((event.axis,int(event.value)),'n')
+			do_every(0.1)
+	ser.close()
